23a51b0cb3adb3b73d9a980f9f363c8d677b7a8f
Ronald Leppink <ronaldleppink@gmail.com>
2017-04-11 22:40:46 +0200

    Cleanup

--- thinkpad-brightness.hs
+++ thinkpad-brightness.hs
@@ -1,12 +1,11 @@
 #!/usr/bin/env runhaskell
 
-
 import System.Environment
 import System.Exit
 
 
 main :: IO ()
-main = getArgs >>= parse >> putStrLn "yo?"
+main = getArgs >>= parse
 
 
 parse :: [String] -> IO String
@@ -15,44 +14,38 @@ parse ["-h"]    = usage                              >> exitSuccess
 parse ["-v"]    = version                            >> exitSuccess
 parse ["-i"]    = specify "-i"                       >> exitSuccess
 parse ["-d"]    = specify "-d"                       >> exitSuccess
-parse ["-i", x] = changeBrightness (read x :: Int) >> exitSuccess
+parse ["-i", x] = changeBrightness (read x :: Int)   >> exitSuccess
 parse ["-d", x] = changeBrightness (negate $ read x :: Int) >> exitSuccess
-
 
 backlightFilePath :: String -> String
 backlightFilePath filename  = "/sys/class/backlight/intel_backlight/" ++ filename
 
-
 changeBrightness :: Int -> IO ()
 changeBrightness x = do
     brightnessFileContents <- readFile $ backlightFilePath "brightness"
-    seq (length brightnessFileContents) (return())
+    seq (length brightnessFileContents) (return())  -- Stupid hack
     maxBrightnessFileContents <- readFile $ backlightFilePath "max_brightness"
 
     let currentBrightness = read brightnessFileContents :: Int
     let maxBrightness = read maxBrightnessFileContents :: Int
 
-    let newBrightness = calcDeltaBrightness currentBrightness 10 maxBrightness x
+    let newBrightness = calcBrightnessDelta currentBrightness 10 maxBrightness x
 
     writeFile (backlightFilePath "brightness") $ show newBrightness
 
-
-calcDeltaBrightness :: Int -> Int-> Int -> Int -> Int
-calcDeltaBrightness current min max delta
+calcBrightnessDelta :: Int -> Int-> Int -> Int -> Int
+calcBrightnessDelta current min max delta
   | (newValue == min) || (newValue == max) = newValue
-  | (min < newValue) && (newValue < max) = newValue
-  | newValue > max = max
-  | newValue < min = min
+  | (min < newValue)  && (newValue < max)  = newValue
+  | newValue > max                         = max
+  | newValue < min                         = min
   where newValue = current + delta
-
 
 specify :: String -> IO ()
 specify x = putStrLn $ x ++ " needs an argument"
 
-
 usage :: IO ()
 usage = putStrLn "Usage: tbr [-id] [amount]"
-
 
 version :: IO ()
 version = putStrLn "Thinkpad Brightness v0.1"
